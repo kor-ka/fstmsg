ddefb6eb1c638d526c525e8d77ee9c8b458fdd20
gputintsev <g.putintsev@qiwi.ru>
2017-07-14 12:58:16 +0300

    fix - remove imports

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
-import telegram
-from telegram import Bot
-from telegram import InlineKeyboardMarkup
-from telegram.error import NetworkError, Unauthorized
 from time import sleep
-import paho.mqtt.client as mqtt
-import pykka
-import urllib
-import json
-import os
 
 import BotActor
-import ManagerActor
 
 if __name__ == '__main__':
     BotActor.BotActor.start()
